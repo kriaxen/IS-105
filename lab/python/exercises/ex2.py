69639544eb878347a7c752d0e8a2f92e785e853a
nadia <onlynadia@hotmail.com>
2014-01-22 13:05:35 +0100

    added ex2.py, ex4-py

---
+++ lab/python/exercises/ex2.py
@@ -0,0 +1,7 @@
+#a comment, this is so  yu can read your program later.
+# Anything after the # is ignored by python.
+print "I could have code like this." # and the commit after is igored
+#You can also use a comment to "disable" or comment out a piece of code:
+# print "this won't run."
+print "This will run."
+
