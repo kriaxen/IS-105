d39dfa43c9999dc246db19f0b59804f4e239b826
Kristian <kriaxen@github.com>
2014-01-28 10:46:12 +0100

    Added my name to lab2.py

--- lab/python/lab2.py
+++ lab/python/lab2.py
@@ -11,8 +11,8 @@ import sys
 
 # Skriv inn fullt navn på gruppemedlemene (erstatte '-' med navn slikt 'Kari Trå')
 gruppe = {  'student1': '-', \
-			'student2': '-', \
-            'student3': '-', \
+	    'student2': 'Kristian', \
+
 }
 
 #
